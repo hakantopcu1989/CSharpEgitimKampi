1ea4afd5d2e5eb654b79de88ea76baf62934a397
hakantopcu1989 <89578374+hakantopcu1989@users.noreply.github.com>
2026-07-11 23:22:13 +0300

    Add files via upload

---
+++ CSharpEgitimKampi/CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
+
+
+//int x;  // burdaki x field oluyor
+//public int y { get; set; } // burdaki y property oluyor
+
+//void test()
+//{
+//    int z; // metot içinde tanımlanırsa değişken oluyor
+//}
